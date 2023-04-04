2127468b8b95dea7f750272bd5beec9799898586
BreadSoup <79678268+BreadSoup@users.noreply.github.com>
2023-04-03 22:28:20 -0400

    it mods now

--- Main.cs
+++ Main.cs
@@ -1,37 +1,49 @@
-﻿using MelonLoader;
+﻿using System;
+using System.Threading;
+using System.Runtime.InteropServices;
+
+using HarmonyLib;
+
+using MelonLoader;
+
+using UnityEngine;
+
+using System.Collections;
+
+using SLZ.AI;
+using SLZ.Rig;
+using SLZ.Marrow.Data;
+using BoneLib;
+using BoneLib.BoneMenu;
+
 
 namespace bonelab_template
 {
-    internal partial class Main : MelonMod
-    {
-        public override void OnEarlyInitializeMelon()
-        {
-            base.OnEarlyInitializeMelon();
-        }
 
+internal partial class Main : MelonMod
+    {
         public override void OnInitializeMelon()
         {
-            base.OnInitializeMelon();
+           CreateBoneMenu();
         }
 
-        public override void OnLateInitializeMelon()
+        public static void CreateBoneMenu() //creates bonemenu
         {
-            base.OnLateInitializeMelon();
+
+            var category = MenuManager.CreateCategory("Death", "#e33e32"); //this just makes it so I dont have to add all of this to the begining of every element
+           // category.CreateBoolElement("Mod Toggle", Color.yellow, Main.IsEnabled, new Action<bool>(Main.OnSetEnabled)); later
+
+
+            category.CreateFunctionElement("DIE", Color.red, delegate () //This gets the barcode of the avatar to switch and also sets a button to the name of the avatar
+            {
+                Player.rigManager.health.curr_Health = 2f;
+                Player.rigManager.health._testRagdollOnDeath = true;
+                Player.rigManager.health.TAKEDAMAGE(3458543534);
+
+
+            });
         }
 
-        public override void OnUpdate()
-        {
-            base.OnUpdate();
-        }
-
-        public override void OnFixedUpdate()
-        {
-            base.OnFixedUpdate();
-        }
-
-        public override void OnLateUpdate()
-        {
-            base.OnLateUpdate();
-        }
     }
+
 }
